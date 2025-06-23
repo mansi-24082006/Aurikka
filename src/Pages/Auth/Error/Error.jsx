e6ccf3eebf4c006d5b8fb981981ee29bbb7ecbc8
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-06-23 21:46:56 +0530

    Update Error.jsx

--- src/Pages/Auth/Error/Error.jsx
+++ src/Pages/Auth/Error/Error.jsx
@@ -1,13 +1,1 @@
 
-const Error = () => {
-  return (
-      <div className="lg:container mx-auto flex items-center justify-center">
-          <div className="min-h-[400px] w-full flex items-center justify-center">
-
-              <h3 className="text-4xl text-red-500 font-inter font-bold capitalize">Not Found This Page!</h3>
-          </div>
-      </div>
-  );
-};
-
-export default Error;
