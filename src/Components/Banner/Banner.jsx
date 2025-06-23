1127654ab6ae3fe079386f8af8d34a000990c0b5
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-06-23 21:49:05 +0530

    Create Banner.jsx

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Shine Bright Like a Diamond💎</h2>
+        <p>
+        ✨ Discover elegance that never fades — where every piece tells a story. 💍 Timeless beauty, crafted just for you. 🛍️ Shop now & shine brighter than ever! ➤
+        </p>
+        <button>
+        Explore Collection <span style={{ color: "#000000" }}>➤</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
